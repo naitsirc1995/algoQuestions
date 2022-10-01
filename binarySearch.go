1d6eccfa5e565c2977d921d0d310213fc4331a8b
cdchavezq@unal.edu.co <cdchavezq@unal.edu.co>
2022-10-01 11:47:48 -0300

    Added binary search in golang

---
+++ binarySearch.go
@@ -0,0 +1,21 @@
+package main
+
+func BinarySearch(array []int, target int) int {
+
+	bottom := 0
+	top := len(array) - 1
+
+	for bottom <= top {
+		mIdx := (top + bottom) / 2
+		if array[mIdx] < target {
+			bottom = mIdx + 1
+		} else if array[mIdx] > target {
+			top = mIdx - 1
+		}
+
+		if array[mIdx] == target {
+			return mIdx
+		}
+	}
+	return -1
+}
